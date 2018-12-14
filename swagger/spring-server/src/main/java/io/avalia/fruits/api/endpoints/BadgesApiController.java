491dac8a04286e394aeb87d7d309e02b679b5498
romain_silvestri <romain.silvestri@heig-vd.ch>
2018-12-14 10:27:16 +0100

    c'est trop tard

--- swagger/spring-server/src/main/java/io/avalia/fruits/api/endpoints/BadgesApiController.java
+++ swagger/spring-server/src/main/java/io/avalia/fruits/api/endpoints/BadgesApiController.java
@@ -1,6 +1,6 @@
 package io.avalia.fruits.api.endpoints;
 
-import io.avalia.fruits.api.BadgeApi;
+import io.avalia.fruits.api.BadgesApi;
 import io.avalia.fruits.entities.BadgeEntity;
 import io.avalia.fruits.api.model.Badge;
 import io.avalia.fruits.repositories.BadgeRepository;
@@ -8,42 +8,20 @@ import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import javax.websocket.server.PathParam;
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2017-07-26T19:36:34.802Z")
 
 @Controller
-public class BadgesApiController implements BadgeApi {
+public class BadgesApiController implements BadgesApi {
 
     // TODO: utiliser la appId pour récupérer les badges seulement de l'app voulue
 
     @Autowired
     BadgeRepository badgeRepository;
-
-    public ResponseEntity<Object> createBadge(@ApiParam(value = "" ,required=true ) @RequestBody Badge badge) {
-        BadgeEntity newBadgeEntity = toBadgeEntity(badge);
-        badgeRepository.save(newBadgeEntity);
-        Long id = newBadgeEntity.getId();
-
-        // TODO: check si jamais c'est la bonne méthode car pas sur
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest().path("/{id}")
-                .buildAndExpand(newBadgeEntity.getId()).toUri();
-
-        // Old version
-        // URI location = ServletUriComponentsBuilder.getFromCurrentRequest().path("/{id}").buildeAndExpand(newBadgeEntity.getId()).toUri();
-
-        return ResponseEntity.created(location).build();
-    }
 
     public ResponseEntity<List<Badge>> getBadges(@ApiParam(value = "" ,required=true ) @RequestHeader(value="appKey", required=true) Integer appKey) {
         List<Badge> badges = new ArrayList<>();
@@ -53,12 +31,6 @@ public class BadgesApiController implements BadgeApi {
 
         return ResponseEntity.ok(badges);
     }
-
-     public ResponseEntity<Badge> modifiyBadge(@ApiParam(value = "" ,required=true ) @RequestBody Badge badge,
-                                               @ApiParam(value = "",required=true ) @PathVariable("badgeName") String badgeName){
-        // TODO: a faire
-        return null;
-     }
 
 
     private BadgeEntity toBadgeEntity(Badge badge) {
